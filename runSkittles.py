946ccd3997935e2b5643dccf401d4eed537eb29a
rpb <robertbennett@projectfla.com>
2015-02-10 11:22:48 -0500

    Fixed variable names the example code in runSkittles.

--- runSkittles.py
+++ runSkittles.py
@@ -1,7 +1,20 @@
+
+#Flat prior
 Model1 = np.linspace(0, 1, 100)
 prior1 = [0.01]*100
+#normal distribution 
 Model2 = np.linspace(0, 1, 100)
 prior2 = stat.norm.pdf(range(-50,50),0.5, 2)
 Model3 = np.linspace(0, 1, 1000)
 prior3 = stat.norm.pdf(range(-500,500),0.5, 2)
-plt.plot(Model, prior)
+plt.plot(Model1, prior1)
+
+
+#Update prob using cumulative data
+runLoop(Model1, prior1, 10, 1)
+
+#Update prob using last 1 draws
+runLoop2(Model1, prior1, 10, 1)
+
+#Update prob using last 5 draws
+runLoop2(Model1, prior1, 10, 5)
