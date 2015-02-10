4e13e2be95a99146da055c50c93842b576f28b67
rpb <robertbennett@projectfla.com>
2015-02-10 11:37:19 -0500

    removed extra plot of prior and added underscore to function calls

--- runSkittles.py
+++ runSkittles.py
@@ -1,4 +1,4 @@
-
+from Skittles import *
 #Flat prior
 Model1 = np.linspace(0, 1, 100)
 prior1 = [0.01]*100
@@ -7,14 +7,12 @@ Model2 = np.linspace(0, 1, 100)
 prior2 = stat.norm.pdf(range(-50,50),0.5, 2)
 Model3 = np.linspace(0, 1, 1000)
 prior3 = stat.norm.pdf(range(-500,500),0.5, 2)
-plt.plot(Model1, prior1)
-
 
 #Update prob using cumulative data
 runLoop(Model1, prior1, 10, 1)
 
 #Update prob using last 1 draws
-runLoop2(Model1, prior1, 10, 1)
+runLoop_2(Model1, prior1, 10, 1)
 
 #Update prob using last 5 draws
-runLoop2(Model1, prior1, 10, 5)
+runLoop_2(Model1, prior1, 10, 5)
